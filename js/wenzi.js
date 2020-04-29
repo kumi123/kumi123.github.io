29e515158da3a575a96c9d2c213f2cfd9f3fa012
kumi123 <1643340114@qq.com>
2020-04-29 21:16:04 +0800

    Site updated: 2020-04-29 21:16:00

--- js/wenzi.js
+++ js/wenzi.js
@@ -3,7 +3,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         // var a = new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");
-        var a = new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");
+        var a = new Array("富强","❤KJ❤KJ","民主","❤KJ❤KJ","文明","❤KJ❤KJ","和谐","❤KJ❤KJ","自由","❤KJ❤KJ","平等","❤KJ❤KJ","公正","❤KJ❤KJ","法治","❤KJ❤KJ","爱国","❤KJ❤KJ","敬业","❤KJ❤KJ","诚信","❤KJ❤KJ","友善","❤KJ❤KJ");
         var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
